864f72a948860e5a8f6479a4e9f10208f54105bc
Georgi Angelov <gtangelov76@gmail.com>
2026-01-31 16:14:28 +0200

    Update center_point.py

--- center_point.py
+++ center_point.py
@@ -1,12 +1,12 @@
+from math import floor
 def main(first_x, first_y, second_x, second_y):
 
     first_distance = first_x ** 2 + first_y ** 2
     second_distance = second_x ** 2 + second_y ** 2
 
     if first_distance <= second_distance:
-        return int(first_x), int(first_y)
-    else:
-        return int(second_x), int(second_y)
+        return floor(first_x), floor(first_y)
+    return floor(second_x), floor(second_y)
 
 first_dot_x = float(input())
 first_dot_y = float(input())
@@ -15,4 +15,4 @@ second_dot_y = float(input())
 
 result = main(first_dot_x, first_dot_y, second_dot_x, second_dot_y)
 
-print(f"{result}")
+print(result)
